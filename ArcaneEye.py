21c51e95996f898f669536a53c8cee3fc78e0b48
Chauncey Meade <cmeade91@gmail.com>
2023-10-20 23:12:15 -0400

    Removing 2nd parameter for now, will work on it after I've slept

--- ArcaneEye.py
+++ ArcaneEye.py
@@ -47,9 +47,7 @@ def open_eye():
 	@eye.tree.command(name="card", description="Display an image of the requested card. Spelling must be exact, but the name isn't case sensitive.")
 	@app_commands.describe(card_name="The exact name of the card.")
 	@app_commands.rename(card_name="name")
-	@app_commands.choices(public=[app_commands.Choice(name="true", value=1),app_commands.Choice(name="false", value=2)])
-	async def show_card(interaction: discord.Interaction, card_name: str, public: app_commands.Choice[int]):
-		public = True if public is 1 else False
+	async def show_card(interaction: discord.Interaction, card_name: str):
 		if card_name in cards: #sends image if card name is valid
 			await interaction.response.send_message(f"{cards[card_name.lower()]['Link']}", ephemeral= not public)
 		else: #responds to just the caller to let them know a card wasn't found if name is invalid
